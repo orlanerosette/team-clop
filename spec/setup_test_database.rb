ecb6b19de88f7c886525ddff4d1c551cfd75dd0e
Laetitia Studer <67235684+laetitiastuder@users.noreply.github.com>
2022-04-06 12:37:18 +0100

    Update setup_test_database.rb

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -2,5 +2,5 @@ require 'pg'
 
 def setup_test_database
   connection = PG.connect(dbname: 'makersbnb_test')
-  connection.exe("TRUNCATE accounts, listings, availability, bookings;")
+  connection.exec("TRUNCATE accounts, listings, availability, bookings;")
 end
